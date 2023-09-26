2051b8c5de4846c7fcbc1ab0b72f786092e504d6
Miguel Negron <manegron@redborder.com>
2023-09-26 22:50:35 +0100

    Match metadata version with tag

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'arodriguez@redborder'
 license          'AGPLv3'
 description      'Installs/Configures Minio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.0.3'
