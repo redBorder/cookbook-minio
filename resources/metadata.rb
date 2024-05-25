4033c8ec9fd92eeadb8b178b58289da054fa004f
Miguel Negron <manegron@redborder.com>
2024-05-26 00:21:00 +0100

    [WIP] Fix cookbook lint metadata.rb

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -1,7 +1,6 @@
 name             'minio'
-maintainer       'Alberto Rodríguez'
-maintainer_email 'arodriguez@redborder'
-license          'AGPLv3'
+maintainer       'Miguel Negrón'
+maintainer_email 'manegron@redborder.com'
+license          'AGPL-3.0'
 description      'Installs/Configures Minio'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.4'
