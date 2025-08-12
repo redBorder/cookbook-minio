af5528241b328a9efb35d94807c3f6c43e78fef4
Pablo Pérez <pperez@redborder.com>
2025-08-12 12:56:48 +0100

    Add malware attributes

--- resources/attributes/default.rb
+++ resources/attributes/default.rb
@@ -9,7 +9,9 @@ default['minio']['s3_user'] = ['redborder']
 default['minio']['s3_password'] = ['redborder']
 default['minio']['s3_hosts'] = ['localhost:9000']
 default['minio']['s3_bucket'] = 'bucket'
+default['minio']['s3_malware_bucket'] = 'malware'
 default['minio']['s3_endpoint'] = 's3.service'
+default['minio']['s3_malware_endpoint'] = 'malware.s3.service'
 
 # Flags
 default['minio']['registered'] = false
